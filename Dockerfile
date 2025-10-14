282b5bd9ae71edf5d1576ae0fca839c39f3bdc56
Anuj Walia <anuj.walia@astronomer.io>
2025-10-14 19:40:58 -0400

    Commits DBT based dag using airflow-dbt-python library

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 #FROM astrocrpublic.azurecr.io/runtime:3.1-1
-FROM quay.io/astronomer/astro-runtime:13.2.0-ubi9-python-3.11
+FROM quay.io/astronomer/astro-runtime:13.1.0
+#-ubi9-python-3.11
--- dags/basic.py
+++ dags/basic.py
@@ -1,6 +1,8 @@
 from airflow.decorators import dag, task
 from datetime import datetime
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 @dag(schedule=None,start_date=datetime(2025,10,1),catchup=False)
 def dag_basic_with_decorators():
 
@@ -12,6 +14,7 @@ def dag_basic_with_decorators():
     @task()
     def work():
         print("Executing work")
+        logging.debug("**************debug work*****************")
 
     @task()
     def stop():
